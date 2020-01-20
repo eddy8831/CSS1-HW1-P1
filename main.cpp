77304df33fac83ebeb411de38564504c9da4f8dc
eddy8831 <f.eduardo12358@gmail.com>
2020-01-20 13:01:02 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
-/* Title average.cpp
+/*
+ * Title: average.cpp
  * Description: This program computes average of
  *       three exams.
  * Author: Eduardo Flores Aguirre
@@ -8,34 +9,16 @@
 using namespace std;
 int main ()
 {
-    int cents;
-    cout << "Enter number of cents " << endl;
-    cin >> cents ;
-    
-    int quarter = cents / 25;
-    cents = cents % 25 ;
-    
-    int dimes = cents / 10;
-    cents = cents % 10 ;
-    
-    int nickels = cents / 5;
-    cents = cents % 5 ;
-    
-    int pennies = cents / 1;
-    cents = cents % 5;
-    
-    
-    
-    
-
-
-    cout << "quaters "<< quarter << endl;
-    cout << "dimes "<< dimes << endl;
-    cout << "nickles "<< nickels << endl;
-    cout << "pennies "<< pennies << endl;
-   
-    
-    
-
+    int num1;
+    int num2;
+    cout << "Enter two Numbers " << endl;
+    cin >> num1;
+    cin >> num2;
+    int sum = (num1 + num2 );
+    int sum2 = (num1 + num2 ) /2;
+    int sum3 = (num1 * num2);
+    cout << "  "<< num1 << " + " << num2 << " = "      << sum << endl;
+    cout << "  "<< num1 << " + " << num2 << " / 2 = " << sum2 << endl;
+    cout << "  "<< num1 << " * " << num2 << " = "      << sum3 << endl;
     return 0;
 }
